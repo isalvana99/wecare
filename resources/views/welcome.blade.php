36a61c8f0c271496bd9cba495aa2379af28634f2
ardenSupremacy <solisarden30@gmail.com>
2022-04-26 14:39:00 +0800

    arden v2.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    arden v2
+                                    arden v2.1
                                 </div>
                             </div>
                         </div>
