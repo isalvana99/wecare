8fc080125cdbf63a7ce7993807c6eae365116602
ardenSupremacy <solisarden30@gmail.com>
2022-04-26 14:59:57 +0800

    for ivy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,11 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-
-                                    arden v2.1
-
-                                    PULL PUSH BY ARDEN SOLIS
-
+                                        for ivy
                                 </div>
                             </div>
                         </div>
